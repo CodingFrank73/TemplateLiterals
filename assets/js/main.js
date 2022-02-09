9d27b06139274eeabde17612563e9bbfabe44b50
Frank <codingfrank73@gmail.com>
2022-02-09 15:50:29 +0100

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+
+let pVorname = "Frank";
+let pNachname = "Hauser";
+let pAlter = "49";
+let pGebOrt = "Stuttgart";
+
+console.log(pVorname + " " + pNachname + ", Alter: " + pAlter + ", Geboren in " + pGebOrt);
+document.write(pVorname + " " + pNachname + ", Alter: " + pAlter + ", Geboren in " + pGebOrt + "<br>")
+
+console.log(`${pVorname} ${pNachname}, Alter: ${pAlter}, geboren in ${pGebOrt}.`);
+document.write(`${pVorname} ${pNachname}, Alter: ${pAlter}, geboren in ${pGebOrt}.` + "<br>")
+
+document.write("--------------------------------------------------------------------------------------" + "<br>")
+let lunatic1 = "The lunatic is on";
+let lunatic2 = "The lunatic is in";
+let andIf = "And if";
+
+document.write(`The lunatic is on the grass<br>The lunatic is on the grass<br> 
+Remembering games and daisy chains and laughs<br>Got to keep the loonies on the path<br>`);
+document.write("--------------------------------------------------------------------------------------" + "<br>")
+document.write(`Brain Damage Songtext <br>
+${lunatic1} the grass <br>
+${lunatic1} the grass <br>
+Remembering games and daisy chains and laughs <br>
+Got to keep the loonies on the path <br>
+
+${lunatic2} the hall <br>
+The lunatics are in my hall <br>
+The paper holds their folded faces to the floor <br>
+And every day the paperboy brings more <br>
+
+
+${andIf} the dam breaks open many years too soon <br>
+${andIf} there is no room upon the hill <br>
+${andIf} your head explodes with dark forebodings too <br>
+I'll see you on the dark side of the moon <br>
+
+${lunatic2} my head <br>
+${lunatic2} in my head <br>
+You raise the blade, you make the change <br>
+You rearrange me 'til I'm sane <br>
+You lock the door <br>
+And throw away the key <br>
+There's someone in my head but it's not me <br>
+
+${andIf} the cloud bursts, thunder in your ear <br>
+You shout and no one seems to hear <br>
+And if the band you're in starts playing different tunes <br>
+I'll see you on the dark side of the moon <br>
+`)
+
+// `Brain Damage Songtext
+// The lunatic is on the grass
+// The lunatic is on the grass
+// Remembering games and daisy chains and laughs
+// Got to keep the loonies on the path
+
+// The lunatic is in the hall
+// The lunatics are in my hall
+// The paper holds their folded faces to the floor
+// And every day the paperboy brings more
+
+
+// And if the dam breaks open many years too soon
+// And if there is no room upon the hill
+// And if your head explodes with dark forebodings too
+// I'll see you on the dark side of the moon
+
+// The lunatic is in my head
+// The lunatic is in my head
+// You raise the blade, you make the change
+// You rearrange me 'til I'm sane
+// You lock the door
+// And throw away the key
+// There's someone in my head but it's not me
+
+// And if the cloud bursts, thunder in your ear
+// You shout and no one seems to hear
+// And if the band you're in starts playing different tunes
+// I'll see you on the dark side of the moon
+// `
+
